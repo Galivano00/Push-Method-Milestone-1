bcf6bf67809400e4830bf491a10a9ff53c68994e
cokraine244 <141259715+Galivano00@users.noreply.github.com>
2024-12-01 10:18:11 +0700

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>&copy; 2024 Netflify - Built with ❤️</p>
+  </footer>
+);
+
+export default Footer;
